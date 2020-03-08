95fafd590b65a663a4fc68bbb7831e828596bb4f
srongklod <klod9288@gmail.com>
2020-03-09 03:03:40 +0700

    Success!!!

--- app/src/main/java/srongklod_bangtamruat/p/cartoon/MainActivity.java
+++ app/src/main/java/srongklod_bangtamruat/p/cartoon/MainActivity.java
@@ -8,41 +8,48 @@ import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.ImageView;
 
+
 public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
-    private ImageView imageView;
-    Matrix matrix = new Matrix();
     private ScaleGestureDetector radar;
+    private ImageView image1;
+    Matrix matrix = new Matrix();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        radar = new ScaleGestureDetector(this, new ScaleListtenner());
-
-        imageView = findViewById(R.id.imv1);
-        imageView.setOnTouchListener(this);
+        radar = new ScaleGestureDetector(this, new ScaleListener());
+        image1 = (ImageView)findViewById(R.id.imv1);
+        image1.setOnTouchListener(this);
 
     }
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
+
         radar.onTouchEvent(event);
-        ImageView view = (ImageView) v;
+
+        ImageView view = (ImageView)v;
         view.setImageMatrix(matrix);
+
         return true;
     }
 
-    private class ScaleListtenner extends ScaleGestureDetector.SimpleOnScaleGestureListener {
-        @Override
-        public boolean onScale(ScaleGestureDetector detector) {
-            float scaleFactor = detector.getScaleFactor();
-            matrix.postScale(scaleFactor, scaleFactor);
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener{
 
+        @Override
+        public boolean onScale(ScaleGestureDetector radar){
+
+            float scaleFactor = radar.getScaleFactor();
+            matrix.postScale(scaleFactor, scaleFactor)  ;
 
             return true;
         }
-    }
 
+    }
 }
+
+
